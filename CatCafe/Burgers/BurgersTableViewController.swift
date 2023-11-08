a8d469f03c56292ca10ceeab3ef874a09d5fbbc4
LeraCuk <2chka.net@gmail.com>
2023-11-08 21:55:42 +0200

    Create MenuController, TitleController. Added buttons on it, appearance of controllers, buttons, titles. Added BurgerTableViewController.

---
+++ CatCafe/Burgers/BurgersTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BurgersViewController.swift
+//  CatCafe
+//
+//  Created by Lera Cuk on 08.11.2023.
+//
+
+import UIKit
+
+class BurgersTableViewController: UITableViewController{
+    
+    @IBOutlet var burgersUITableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
