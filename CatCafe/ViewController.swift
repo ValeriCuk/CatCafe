733662cac527bb4dbc25e10971536e8db1da6ff0
LeraCuk <2chka.net@gmail.com>
2023-11-08 12:45:25 +0200

    New project

---
+++ CatCafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatCafe
+//
+//  Created by Lera Cuk on 08.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
