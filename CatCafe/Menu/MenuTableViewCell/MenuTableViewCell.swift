d2fd4618511428ed65751794dac50455da021b86
LeraCuk <2chka.net@gmail.com>
2023-11-10 14:17:01 +0200

    correction constraint menu cell

--- CatCafe/Menu/MenuTableViewCell/MenuTableViewCell.swift
+++ CatCafe/Menu/MenuTableViewCell/MenuTableViewCell.swift
@@ -11,6 +11,7 @@ class MenuTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         setupConstraints()
+        addDescriptionLabelFrame()
     }
     
     override func layoutSubviews() {
@@ -18,27 +19,31 @@ class MenuTableViewCell: UITableViewCell {
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
     }
     
+    private func addDescriptionLabelFrame(){
+        descriptionLabel.numberOfLines = 0
+    }
+    
     private func setupConstraints() {
         
         nameLabel.snp.makeConstraints{ make in
-            make.top.equalTo( contentView.snp.top).offset(15)
+            make.top.equalTo( contentView.snp.top).offset(5)
             make.left.equalTo(itemImage.snp.right).offset(5)
             make.right.equalTo(contentView.snp.right).offset(-16)
-            make.bottom.equalTo(descriptionLabel.snp.top).offset(-15)
-            make.height.equalTo(30)
+            make.bottom.equalTo(descriptionLabel.snp.top).offset(-5)
+            make.height.equalTo(25)
         }
         
         itemImage.snp.makeConstraints{ make in
-            make.top.equalTo(contentView.snp.top).offset(20)
-            make.left.equalTo(contentView.snp.left).offset(5)
-            make.right.equalTo(descriptionLabel.snp.left).offset(-20)
+            make.top.equalTo(contentView.snp.top).offset(16)
+            make.left.equalTo(contentView.snp.left).offset(10)
+            make.right.equalTo(descriptionLabel.snp.left).offset(-10)
             make.height.equalTo(100)
             make.width.equalTo(100)
         }
         
         descriptionLabel.snp.makeConstraints{ make in
             make.right.equalTo(contentView.snp.right).offset(-16)
-            make.height.equalTo(40)
+            make.height.equalTo(60)
         }
         
         priceLabel.snp.makeConstraints{ make in
